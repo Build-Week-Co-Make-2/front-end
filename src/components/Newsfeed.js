392e013b6f363e88b011725f97df31cdfdf698a4
Theresa M Lennon <oddyseyt@gmail.com>
2020-06-22 20:38:31 -0500

    Working on newsfeed comp

--- src/components/Newsfeed.js
+++ src/components/Newsfeed.js
@@ -1,12 +1,27 @@
 import React, {useState, useEffect} from 'react';
-//import { Card, Button, Form, FormGroup, Label, Input } from 'reactstrap';
-//import axios from 'axios';
+import { Card, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import axios from 'axios';
 
 
 const Newsfeed = props => {
-   // const [feed, setFeed] = useState([]);
+    const [feed, setFeed] = useState([]);
 
+   useEffect(() => {
+      const getNewsfeed = () => {
+      axios
+      .get('https://co-make-9cf46.web.app/api/posts')
+      .then(response => {
+         console.log(response)
+         setFeed(response.data);
 
+      })
+      .catch(error => {
+         console.error('Server error', error);
+
+      })
+      }
+      getNewsfeed()
+   }, []);
 
 
 return (
